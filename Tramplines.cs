4b3986573a28ba621ec9397d41eb7c1f58db399b
dizzwtyc <149867620+dizzwtyc@users.noreply.github.com>
2023-12-02 13:59:23 +0600

    um just tramplines script

---
+++ Tramplines.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tramplines : MonoBehaviour
+{
+    public float jumpFactor = 2.5f;
+    
+    void OnTriggerEnter(Collider other)
+    {
+        // Увеличение силы прыжкагрока
+
+        other.GetComponent<Jump>().jumpStrength *= jumpFactor;
+    }
+    void OnTriggerExit(Collider other)
+    {
+        //Понижение силы прыжка
+
+        other.GetComponent<Jump>().jumpStrength /= jumpFactor;
+
+    }
+}
+    
